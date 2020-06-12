219c910072ed35bc0bd0a7c8595101489a1267a9
wiebsS <wi-stein@web.de>
2020-06-12 15:20:40 +0200

    inspect functionarguments

---
+++ snippets/inspectortest.py
@@ -0,0 +1,16 @@
+import inspect
+
+def aFunction(bla=123, blub='abc'):
+    pass
+
+def varnames(f):
+    """
+    Given a functionname f, it returns a tuple 
+    ArgSpec(args, varargs, keywords, defaults),
+    where args is a list of the parameter names
+    """
+    return inspect.getargspec(f)[0]
+
+
+print(varnames(aFunction))
+print("erster Argumentenname: " + (varnames(aFunction)[0]))
