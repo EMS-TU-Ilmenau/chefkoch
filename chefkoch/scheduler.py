b966b4f05d48bae4262a1a2ae727f8fe8ce6b68b
wiebsS <wi-stein@web.de>
2020-08-04 11:34:59 +0200

    mini-update scheduler

--- chefkoch/scheduler.py
+++ chefkoch/scheduler.py
@@ -1,15 +1,9 @@
 import chefkoch
 
+class Worker:
 
-class Job:
-    """
-    A job which represents the execution of a step.
-    It is scheduled by the scheduler
-    """
-
-    def __init__(self, scheduler, step):
+    def __init__(self, scheduler):
         self.scheduler = scheduler
-        self.setp = step
 
 
 class Scheduler:
